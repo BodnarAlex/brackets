febf09f7c78ac66d4a2992a88016a875c92cc931
Bodnar Alex <allugovskova@mail.ru>
2023-08-19 18:18:46 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newArr = bracketsConfig.map((x)=>x.join(''));
+  while (str!='' && newArr.some(e => str.includes(e))){
+    for(let j =0; j<newArr.length; j++){
+      str = str.replaceAll(newArr[j], "");
+    }
+  }
+  return (str === "");
 }
